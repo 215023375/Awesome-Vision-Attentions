f404f89759a01b6332e991512682a2abae995607
Nkosenhle Duma <personal.godsonduma@gmail.com>
2023-01-05 12:24:25 +0200

    Using https://amaarora.github.io/2020/07/24/SeNet.html\#squeeze-and-excitation-block-in-pytorch SE_Block

--- code/channel_attentions/se_module.py
+++ code/channel_attentions/se_module.py
@@ -4,20 +4,20 @@ import numpy as np
 from torch import nn
 
 class SELayer(nn.Module):
-    def __init__(self, channel, reduction=16):
-        super(SELayer, self).__init__()
-        self.avg_pool = nn.AdaptiveAvgPool2d(1)
-        self.fc = nn.Sequential(
-            nn.Linear(channel, channel // reduction, bias=False),
-            nn.ReLU(),
-            nn.Linear(channel // reduction, channel, bias=False),
+    def __init__(self, c, r=16):
+        super().__init__()
+        self.squeeze = nn.AdaptiveAvgPool2d(1)
+        self.excitation = nn.Sequential(
+            nn.Linear(c, c // r, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(c // r, c, bias=False),
             nn.Sigmoid()
         )
 
     def forward(self, x):
-        # b, c = x.size()
-        y = self.avg_pool(x)
-        y = self.fc(y)
+        bs, c, _, _ = x.shape
+        y = self.squeeze(x).view(bs, c)
+        y = self.excitation(y).view(bs, c, 1, 1)
         return x * y.expand_as(x)
 
 
