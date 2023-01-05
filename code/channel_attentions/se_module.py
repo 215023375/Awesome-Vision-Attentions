d4a9cd8071bbe83f6cd31c86822d7ac2685024b0
Nkosenhle Duma <personal.godsonduma@gmail.com>
2023-01-05 11:48:37 +0200

    Reverting se_module updates

--- code/channel_attentions/se_module.py
+++ code/channel_attentions/se_module.py
@@ -1,36 +1,16 @@
 from typing import Optional, Callable
 
-
+import numpy as np
 from torch import nn
-from torchvision.models.resnet import BasicBlock
 
-
-class SELayer(BasicBlock):
-    def __init__(self,
-        inplanes: int,
-        planes: int,
-        stride: int = 1,
-        downsample: Optional[nn.Module] = None,
-        groups: int = 1,
-        base_width: int = 64,
-        dilation: int = 1,
-        norm_layer: Optional[Callable[..., nn.Module]] = None,
-    ) -> None:
-        super(SELayer, self).__init__(
-            inplanes,
-            planes,
-            stride=stride,
-            downsample=downsample,
-            groups=groups,
-            base_width=base_width,
-            dilation=dilation,
-            norm_layer=norm_layer,
-        )
-        self.avg_pool = nn.AdaptiveAvgPool2d(self.expansion)
+class SELayer(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.fc = nn.Sequential(
-            nn.Linear(inplanes, inplanes // planes, bias=False),
+            nn.Linear(channel, channel // reduction, bias=False),
             nn.ReLU(),
-            nn.Linear(inplanes // planes, self.expansion * inplanes, bias=False),
+            nn.Linear(channel // reduction, channel, bias=False),
             nn.Sigmoid()
         )
 
@@ -43,7 +23,7 @@ class SELayer(BasicBlock):
 
 def main():
     attention_block = SELayer(64)
-    input = jt.rand([4, 64, 32, 32])
+    input = np.rand([4, 64, 32, 32])
     output = attention_block(input)
     print(input.size(), output.size())
 
